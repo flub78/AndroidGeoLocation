97d33ffb5e0975916b27de8e1b020eb049c25d06
Flub78 <frederic.peignot@free.fr>
2021-03-24 20:57:54 +0100

    Functional with geo decoding of the address

--- app/src/main/java/com/letssurf/geolocation/MainActivity.java
+++ app/src/main/java/com/letssurf/geolocation/MainActivity.java
@@ -1,6 +1,8 @@
 package com.letssurf.geolocation;
 
 import android.Manifest;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -15,21 +17,30 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FetchAddressTask.OnTaskCompleted {
 
     static final String TAG = "MainActivity";
     public static final int REQUEST_LOCATION_PERMISSION = 99;
     private static final String TRACKING_LOCATION_KEY = "tracking_location";
 
     private ImageView mAndroidImageView;
+    private AnimatorSet mRotateAnim;
     private TextView mLocationTextView;
     private Button mLocationButton;
 
     private Location mLastLocation;
     private FusedLocationProviderClient mFusedLocationClient;
+
+    private boolean mTrackingLocation = false;
+
+    private LocationCallback mLocationCallback;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +50,21 @@ public class MainActivity extends AppCompatActivity {
         mLocationButton = (Button) findViewById(R.id.button_location);
         mLocationTextView = (TextView) findViewById(R.id.textview_location);
         mAndroidImageView = (ImageView) findViewById(R.id.imageview_android);
+        mRotateAnim = (AnimatorSet) AnimatorInflater.loadAnimator
+                (this, R.animator.rotate);
 
         // Initialize the FusedLocationClient.
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                // If tracking is turned on, reverse geocode into an address
+                if (mTrackingLocation) {
+                    new FetchAddressTask(MainActivity.this, MainActivity.this).execute(locationResult.getLastLocation());
+                }
+            }
+        };
     }
 
     /**
@@ -49,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void onClick(View view) {
+
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -57,16 +81,23 @@ public class MainActivity extends AppCompatActivity {
                             {Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_LOCATION_PERMISSION);
 
+            // when there is no permission the tracking start is postponed
+            // until the permission request result
         } else {
             Log.d(TAG, "getLocation: permissions granted");
-            getLocation();
+
+             if (mTrackingLocation) {
+                stopLocationTracking();
+            } else {
+                startLocationTracking();
+            }
         }
     }
 
     /**
-     *
+     * Start tracking
      */
-    private void getLocation() {
+    private void startLocationTracking() {
 
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
@@ -82,27 +113,55 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "getLocation: permissions not granted");
             return;
         }
-        Log.d(TAG, "getLocation: permissions granted");
-
+        Log.d(TAG, "The getLocation: permissions is granted");
+/*
         mFusedLocationClient.getLastLocation().addOnSuccessListener(
                 new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
                         if (location != null) {
                             mLastLocation = location;
+                            // Start the reverse geocode AsyncTask
+                            new FetchAddressTask(MainActivity.this,
+                                    MainActivity.this).execute(location);
+                            *//*
                             mLocationTextView.setText(
                                     getString(R.string.location_text,
                                             mLastLocation.getLatitude(),
                                             mLastLocation.getLongitude(),
                                             mLastLocation.getTime()));
+
+                             *//*
+
                         } else {
                             mLocationTextView.setText(R.string.no_location);
                         }
                     }
 
-                });
+                }); */
 
+        mFusedLocationClient.requestLocationUpdates
+                (getLocationRequest(), mLocationCallback,
+                        null /* Looper */);
 
+        // mLocationTextView.setText(getString(R.string.address_text, getString(R.string.loading), System.currentTimeMillis()));
+
+        mRotateAnim.start();
+        mTrackingLocation = true;
+        mLocationButton.setText(R.string.stop_tracking);
+    }
+
+    /**
+     * Stop tracking
+     */
+    private void stopLocationTracking () {
+        if (mTrackingLocation) {
+            mRotateAnim.end();
+            mTrackingLocation = false;
+            mLocationTextView.setText(R.string.textview_hint);
+            mLocationButton.setText(R.string.start_tracking);
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
     }
 
     /**
@@ -125,12 +184,38 @@ public class MainActivity extends AppCompatActivity {
                 // otherwise, show a Toast
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getLocation();
+                    startLocationTracking();
                 } else {
                     Toast.makeText(this,
                             R.string.location_permission_denied,
                             Toast.LENGTH_SHORT).show();
                 }
         }
+    }
+
+    /**
+     * Update location text view with the result of the geo coding task
+     * translation from coordinate to text address
+     * @param result
+     */
+    @Override
+    public void onTaskCompleted(String result) {
+        // Update the UI
+        if (mTrackingLocation)
+            mLocationTextView.setText(getString(R.string.address_text,
+                result, System.currentTimeMillis()));
+    }
+
+    /**
+     * Generate a location request with correct parameters
+     * not too slow, not too fast, GPS coordinate if possible
+     * @return
+     */
+    private LocationRequest getLocationRequest () {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
     }
 }
